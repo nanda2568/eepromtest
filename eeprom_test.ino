327c46cb25f9fb4e2e4fd754eb2ea145d7907557
Nanda Kumar Nair <nanda2568@hotmail.com>
2018-03-25 18:41:25 -0500

    Add files via upload

---
+++ eeprom_test.ino
@@ -0,0 +1,41 @@
+#include <EEPROM.h>
+#include <String.h>
+
+String inputStr;
+int idx1, idx2, addr, value;
+String errMsg = "Incorrect format. Eg: read 3, write 3 10";
+
+void setup() {
+  int addr;
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+      inputStr = Serial.readString();   // Example input strings: read 3, write 3 10
+      if (inputStr.substring(0,4) == "read") {
+        idx1 = inputStr.indexOf(" ");
+        if (idx1 == 4) {
+          addr = inputStr.substring(5).toInt();
+          Serial.print("Read from EEPROM: ");
+          Serial.println(EEPROM.read(addr), DEC);
+          
+        } else {
+                  Serial.println(errMsg);
+        }
+      } else if (inputStr.substring(0,5) == "write") {
+              idx1 = inputStr.indexOf(" ");
+              if (idx1 == 5) {
+                idx2 = inputStr.indexOf(" ", idx1+1);
+                addr = inputStr.substring(idx1+1, idx2).toInt();
+                value = inputStr.substring(idx2+1).toInt();
+                EEPROM.write(addr, value);
+                Serial.println("Write to EEPROM address is completed");
+              } else {
+                  Serial.println(errMsg);
+                }
+              } else {
+                  Serial.println(errMsg);
+              }
+  } 
+}
